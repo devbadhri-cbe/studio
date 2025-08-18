5a01e54aa92cc5ff381e8ed03359a5f1c40d4c19
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-08-18 07:49:12 +0000

    I again tried to import data, but showing runtime error.   FirebaseError

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth, Auth } from 'firebase/auth';
+import { getFirestore, Firestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration is automatically provided by App Hosting.
 const firebaseConfig = {
-  projectId: "glycemic-guardian-6uxyg",
-  appId: "1:1023747133263:web:fc7ad4f2a467dad6c9ff3a",
-  storageBucket: "glycemic-guardian-6uxyg.firebasestorage.app",
-  apiKey: "AIzaSyDRROv81qKWJOF1DlsskWYTzWcyYOXvnl4",
-  authDomain: "glycemic-guardian-6uxyg.firebaseapp.com",
-  messagingSenderId: "1023747133263",
+  projectId: 'glycemic-guardian-6uxyg',
+  appId: '1:1023747133263:web:fc7ad4f2a467dad6c9ff3a',
+  storageBucket: 'glycemic-guardian-6uxyg.firebasestorage.app',
+  apiKey: 'AIzaSyDRROv81qKWJOF1DlsskWYTzWcyYOXvnl4',
+  authDomain: 'glycemic-guardian-6uxyg.firebaseapp.com',
+  messagingSenderId: '1023747133263',
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
-const auth = getAuth(app);
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
+
+// Initialize Firebase only once
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+auth = getAuth(app);
+db = getFirestore(app);
 
 export { db, auth, app };
