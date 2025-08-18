242f6bfd2852ea557569bbf816e93d6696939e7b
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-08-18 06:54:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration is automatically provided by App Hosting.
 const firebaseConfig = {
-  "projectId": "glycemic-guardian-6uxyg",
-  "appId": "1:1023747133263:web:fc7ad4f2a467dad6c9ff3a",
-  "storageBucket": "glycemic-guardian-6uxyg.appspot.com",
-  "apiKey": "AIzaSyDRROv81qKWJOF1DlsskWYTzWcyYOXvnl4",
-  "authDomain": "glycemic-guardian-6uxyg.firebaseapp.com",
-  "messagingSenderId": "1023747133263"
+  projectId: "glycemic-guardian-6uxyg",
+  appId: "1:1023747133263:web:fc7ad4f2a467dad6c9ff3a",
+  storageBucket: "glycemic-guardian-6uxyg.firebasestorage.app",
+  apiKey: "AIzaSyDRROv81qKWJOF1DlsskWYTzWcyYOXvnl4",
+  authDomain: "glycemic-guardian-6uxyg.firebaseapp.com",
+  messagingSenderId: "1023747133263",
 };
 
 // Initialize Firebase
