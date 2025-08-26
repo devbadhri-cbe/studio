55b7a46a915b65f3df5b9f72ec963418afbfb7c0
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-08-26 17:04:21 +0000

    I want to place a logo unique to this app in the title bar. Can you sugg

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,15 +10,31 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path
-        d="M26.9,6.1C24.2,3.4,20.2,2,16,2S7.8,3.4,5.1,6.1C2.4,8.8,1,12.8,1,17s1.4,8.2,4.1,10.9S11.8,31,16,31s8.2-1.4,10.9-4.1S31,21.2,31,17S29.6,8.8,26.9,6.1z M25.5,26.5c-2.4,2.4-5.6,3.5-9.5,3.5s-7.1-1.2-9.5-3.5C4.2,24.1,3,20.9,3,17s1.2-7.1,3.5-9.5C8.9,5.2,12.1,4,16,4s7.1,1.2,9.5,3.5c2.4,2.4,3.5,5.6,3.5,9.5S27.8,24.1,25.5,26.5z"
-        fill="currentColor"
-        fillOpacity="0.2"
-      />
-      <path
-        d="M23,15h-6V9c0-0.6-0.4-1-1-1s-1,0.4-1,1v6H9c-0.6,0-1,0.4-1,1s0.4,1,1,1h6v6c0,0.6,0.4,1,1,1s1-0.4,1-1v-6h6c0.6,0,1-0.4,1-1S23.6,15,23,15z"
-        fill="currentColor"
-      />
+        <path 
+            d="M16 30C16 30 30 24 30 16V6L16 2L2 6V16C2 24 16 30 16 30Z" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            fill="currentColor" 
+            fillOpacity="0.1" 
+        />
+        <path 
+            d="M22 12C22 14.2091 20.2091 16 18 16C15.7909 16 14 14.2091 14 12C14 9.79086 15.7909 8 18 8C19.023 8 19.9478 8.40221 20.6212 9.0756C21.2946 9.749 21.7951 10.6433 21.9515 11.6165" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            transform="translate(-2, 2)"
+        />
+        <path 
+            d="M14 17.6165C14.0485 18.5897 14.549 19.484 15.2224 20.1574C15.8958 20.8308 16.8206 21.233 17.8435 21.233C20.0527 21.233 21.8435 19.4421 21.8435 17.233C21.8435 15.0238 20.0527 13.233 17.8435 13.233" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            transform="translate(-2, 2)"
+        />
     </svg>
   );
 }
