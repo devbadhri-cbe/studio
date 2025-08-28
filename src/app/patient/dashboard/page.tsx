f407f1adffb7ce8874d0a200175f0e87ba185acd
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-08-28 04:24:40 +0000

    I want the patient profile panel to be the first to be seen in the Dashb

--- src/app/patient/dashboard/page.tsx
+++ src/app/patient/dashboard/page.tsx
@@ -126,6 +126,11 @@ export default function PatientDashboard() {
                 </Select>
               </div>
             </div>
+            
+            <div className="grid grid-cols-1 gap-6">
+                 <ProfileCard />
+            </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               <div className="md:col-span-2 lg:col-span-1">
                  <ReminderCard />
@@ -134,14 +139,9 @@ export default function PatientDashboard() {
                 <InsightsCard />
               </div>
             </div>
-            <div className="grid auto-rows-fr grid-cols-1 lg:grid-cols-3 gap-6">
-              <div className="lg:col-span-2">
-                {renderDashboard()}
-              </div>
-              <div className="lg:col-span-1">
-                <ProfileCard />
-              </div>
-            </div>
+            
+            {renderDashboard()}
+
           </div>
         </main>
       </div>
