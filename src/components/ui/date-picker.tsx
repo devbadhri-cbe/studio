8ab6cf8348fcc70cd11f080832643b66ad7d9849
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-01 16:30:55 +0000

    No it did not work. What else can we think?

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -32,7 +32,7 @@ export function DatePicker({
   const [open, setOpen] = React.useState(false)
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal={true}>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
