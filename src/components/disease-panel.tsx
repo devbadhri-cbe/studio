ff3215df88640c74eddac721fc80f525985dcf99
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 11:01:00 +0000

    Make the disease panel to display one card with the agreed aspect ratio

--- src/components/disease-panel.tsx
+++ src/components/disease-panel.tsx
@@ -4,13 +4,31 @@
 import { DiabetesCard } from './diabetes-card';
 import { HypertensionCard } from './hypertension-card';
 import { Card, CardContent } from './ui/card';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import * as React from 'react';
 
 
 export function DiseasePanel() {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
-            <DiabetesCard />
-            <HypertensionCard />
-        </div>
+        <Carousel
+            opts={{
+                align: "start",
+            }}
+            orientation="vertical"
+            className="w-full"
+        >
+            <CarouselContent className="-mt-1 h-[550px]">
+                <CarouselItem className="pt-1 md:basis-1/2">
+                   <div className="p-1">
+                        <DiabetesCard />
+                    </div>
+                </CarouselItem>
+                <CarouselItem className="pt-1 md:basis-1/2">
+                   <div className="p-1">
+                        <HypertensionCard />
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+        </Carousel>
     );
 }
