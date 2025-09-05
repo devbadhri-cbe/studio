c5ba83e252e150ce663fdf7656ffd57bb87abc4e
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 05:32:05 +0000

    Inside our app, all our cards should be in same UI for better user exper

--- src/components/diabetes-card.tsx
+++ src/components/diabetes-card.tsx
@@ -34,7 +34,7 @@ export function DiabetesCard() {
   ].filter(Boolean);
 
   return (
-    <Card className="h-full">
+    <Card className="h-full shadow-xl">
         <CardContent className="p-4 space-y-4">
              <div className="flex items-center justify-between">
                 <div className='flex items-center gap-3 flex-1'>
