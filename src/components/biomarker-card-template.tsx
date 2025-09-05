48f877f68a65ff9340a037c512f150fef5bcb9f2
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 12:03:19 +0000

    The records and the status sections are to be stacked vertically always

--- src/components/biomarker-card-template.tsx
+++ src/components/biomarker-card-template.tsx
@@ -46,7 +46,7 @@ export function BiomarkerCardTemplate({
         {hasRecords ? (
           <div className="flex-1 flex flex-col min-h-0">
               {/* Top Section: Records & Status */}
-              <div className="flex flex-row gap-4 min-h-[120px] items-center">
+              <div className="flex flex-col gap-4 min-h-[120px]">
                   <div className="flex-1 w-full p-2 flex items-center justify-center">
                       {recordsList}
                   </div>
