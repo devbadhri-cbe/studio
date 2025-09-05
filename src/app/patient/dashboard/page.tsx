9e3811230c36e0e4f26370d8f342e5e1f10a3b3b
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 14:00:46 +0000

    Profile card is the first card below the heading

--- src/app/patient/dashboard/page.tsx
+++ src/app/patient/dashboard/page.tsx
@@ -102,11 +102,11 @@ export default function PatientDashboard() {
 
             <div className="space-y-6" id="tour-step-1">
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                 <div className="lg:col-span-2 flex flex-col gap-6">
-                    <MedicalHistoryCard />
-                </div>
                 <div className="lg:col-span-1 flex flex-col gap-6">
                     <ProfileCard />
+                </div>
+                <div className="lg:col-span-2 flex flex-col gap-6">
+                    <MedicalHistoryCard />
                 </div>
               </div>
             </div>
