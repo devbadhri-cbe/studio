b2431650f74982ade172e7ba230ae9036e77c720
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 10:55:13 +0000

    Now when I press the Disease panels button this is what the result. Anal

--- src/components/disease-panel.tsx
+++ src/components/disease-panel.tsx
@@ -8,11 +8,9 @@ import { Card, CardContent } from './ui/card';
 
 export function DiseasePanel() {
     return (
-        <Card>
-            <CardContent className="p-4 grid grid-cols-1 lg:grid-cols-2 gap-6">
-               <DiabetesCard />
-               <HypertensionCard />
-            </CardContent>
-        </Card>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
+            <DiabetesCard />
+            <HypertensionCard />
+        </div>
     );
 }
