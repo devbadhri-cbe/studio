6743ee2c12f686d9ffe2da634e3b5a64d23f0fdb
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 15:53:47 +0000

    Good. Do it and check once more before I test

--- src/components/patient-dashboard.tsx
+++ src/components/patient-dashboard.tsx
@@ -98,14 +98,12 @@ export function PatientDashboard() {
                 </Collapsible>
             </div>
 
-            <div className="space-y-6">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="lg:col-span-1 flex flex-col gap-6">
-                    <ProfileCard />
-                </div>
-                <div className="lg:col-span-2 flex flex-col gap-6">
-                    <MedicalHistoryCard />
-                </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="lg:col-span-1 flex flex-col gap-6">
+                  <ProfileCard />
+              </div>
+              <div className="lg:col-span-2 flex flex-col gap-6">
+                  <MedicalHistoryCard />
               </div>
             </div>
             
