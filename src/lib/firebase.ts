f8cade1882ead3aea538d0651bb7839225c36a33
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-05 02:38:25 +0000

    ok

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   "projectId": "glycemic-guardian-6uxyg",
   "appId": "1:1023747133263:web:fc7ad4f2a467dad6c9ff3a",
-  "storageBucket": "glycemic-guardian-6uxyg.appspot.com",
+  "storageBucket": "glycemic-guardian-6uxyg.firebasestorage.app",
   "apiKey": "AIzaSyDRROv81qKWJOF1DlsskWYTzWcyYOXvnl4",
   "authDomain": "glycemic-guardian-6uxyg.firebaseapp.com",
   "measurementId": "",
