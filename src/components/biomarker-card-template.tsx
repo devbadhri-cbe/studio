fe0d2446acc185366f01fcad51ac50156494aa09
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-09 19:16:53 +0000

    This flex box containing the records - Fit to Content

--- src/components/biomarker-card-template.tsx
+++ src/components/biomarker-card-template.tsx
@@ -131,8 +131,8 @@ export function BiomarkerCardTemplate({
         {hasRecords ? (
           <div className="flex-1 flex flex-col min-h-0">
               {/* Top Section: Records & Status */}
-              <div className={cn("flex-1 flex flex-row gap-4 min-h-0 rounded-lg p-2", getBorderColorClass())}>
-                  <div className="w-full border-2 border-blue-500">
+              <div className={cn("flex flex-row gap-4 min-h-0 rounded-lg p-2 border-2 border-green-500", getBorderColorClass())}>
+                  <div className="border-2 border-blue-500">
                       {RecordsList}
                   </div>
                   <div className="flex-1 w-full flex flex-col items-center justify-center border-2 border-red-500">
