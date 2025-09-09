4e21891aa57578b4f216b91cfbd946dd8ce752ef
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-09 13:47:15 +0000

    There should be maximum 3 biomarker cards rows can open for disease pane

--- src/components/interactive-panel-grid.tsx
+++ src/components/interactive-panel-grid.tsx
@@ -62,7 +62,7 @@ export function InteractivePanelGrid({ children }: InteractivePanelGridProps) {
     }
     
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-full">
             {validChildren.map((child, index) => (
                 <div
                     key={index}
