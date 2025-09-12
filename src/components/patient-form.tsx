81f383a7c1648b5cdeb54afbe5399a09e8a444ca
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-12 17:33:18 +0000

    So that is not the correct way. May be there is a flex container prevent

--- src/components/patient-form.tsx
+++ src/components/patient-form.tsx
@@ -86,29 +86,33 @@ export function PatientForm({ onSubmit, onCancel, isSubmitting, initialData }: P
           )}
         />
         
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
-            <DateInput
-              name="dob"
-              label="Date of Birth"
-              fromYear={new Date().getFullYear() - 120}
-              toYear={new Date().getFullYear()}
-            />
-          <FormField
-            control={formMethods.control}
-            name="gender"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Gender</FormLabel>
-                <FormControl>
-                  <RadioGroup onValueChange={field.onChange} value={field.value} className="flex items-center space-x-4 pt-2">
-                    <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="male" /></FormControl><FormLabel className="font-normal">Male</FormLabel></FormItem>
-                    <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="female" /></FormControl><FormLabel className="font-normal">Female</FormLabel></FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="flex flex-col md:flex-row gap-x-6 gap-y-6">
+            <div className="flex-1">
+                 <DateInput
+                    name="dob"
+                    label="Date of Birth"
+                    fromYear={new Date().getFullYear() - 120}
+                    toYear={new Date().getFullYear()}
+                />
+            </div>
+            <div className="flex-1">
+                 <FormField
+                    control={formMethods.control}
+                    name="gender"
+                    render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Gender</FormLabel>
+                        <FormControl>
+                        <RadioGroup onValueChange={field.onChange} value={field.value} className="flex items-center space-x-4 pt-2">
+                            <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="male" /></FormControl><FormLabel className="font-normal">Male</FormLabel></FormItem>
+                            <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="female" /></FormControl><FormLabel className="font-normal">Female</FormLabel></FormItem>
+                        </RadioGroup>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                    )}
+                />
+            </div>
         </div>
 
         <FormField
