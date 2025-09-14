28ec4a63fd3b77732c8feb6e8c515e567af4ba8e
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-14 05:32:16 +0000

    Can we make the color for these buttons similar

--- src/components/upload-record-dialog.tsx
+++ src/components/upload-record-dialog.tsx
@@ -349,7 +349,7 @@ export function UploadRecordDialog() {
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
       <DialogTrigger asChild>
-        <Button size="sm">
+        <Button size="sm" variant="outline">
             <UploadCloud className="mr-2 h-4 w-4" />
             Upload Result
         </Button>
