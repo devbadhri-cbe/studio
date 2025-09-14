f593a77b124ea40b1d35bc6ead1e29cabe3598ff
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-14 17:09:16 +0000

    We will now remove fixed aspect ratio and check. do not modify any thing

--- src/components/universal-card.tsx
+++ src/components/universal-card.tsx
@@ -24,7 +24,7 @@ export function UniversalCard({
   headerClassName,
 }: UniversalCardProps) {
   return (
-    <Card className={cn("w-full flex flex-col h-full shadow-xl aspect-[4/3]", className)}>
+    <Card className={cn("w-full flex flex-col h-full shadow-xl", className)}>
       {(headerContent || actions) && (
         <CardHeader className={cn(headerClassName)}>
             <div className="flex items-start justify-between">
