bea17fea62f2742f9394bc81dd8b9f6aabfe4ee8
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-14 16:00:41 +0000

    It should be touch enabled also for scrolling horizontally

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -184,7 +185,7 @@ const CarouselItem = React.forwardRef<
       role="group"
       aria-roledescription="slide"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full pr-4",
+        "min-w-0 shrink-0 grow-0 basis-full",
         orientation === "horizontal" ? "" : "pt-4",
         className
       )}
