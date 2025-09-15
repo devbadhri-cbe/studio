633f8f3881957cd1c7e9d5709dc4a36401f00b4b
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-15 04:28:59 +0000

    keep a spacer flex box between red box and yellow box to push them apart

--- src/components/biomarker-card.tsx
+++ src/components/biomarker-card.tsx
@@ -190,11 +190,12 @@ export function BiomarkerCard<T extends Record>({
        {hasRecords ? (
           <div className="flex flex-col flex-1 h-full p-6 pt-0">
             <div className="flex flex-row items-stretch gap-4 rounded-lg">
-                <div className="flex border border-red-500 p-1 rounded-md">
-                    <div className="flex-1 flex flex-col justify-center border border-green-500 rounded-lg p-2">
+                <div className="flex flex-col justify-center border border-red-500 p-1 rounded-md">
+                    <div className="flex-1 flex flex-col justify-center border border-green-500 p-1 rounded-lg">
                         {RecordsList}
                     </div>
                 </div>
+                <div className="flex-1 border border-purple-500"></div>
                 <div className="pl-2 border border-yellow-500 flex flex-col justify-center items-center rounded-lg p-2">
                     <div className="flex border border-blue-500 p-1 rounded-md">
                         {statusContent}
