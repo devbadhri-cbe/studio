5162c47fc0c0d4872af393fea1bd71f691ebf416
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-15 02:22:45 +0000

    Draw blue border around biomarker cards

--- src/components/universal-card.tsx
+++ src/components/universal-card.tsx
@@ -24,7 +24,7 @@ export function UniversalCard({
   headerClassName,
 }: UniversalCardProps) {
   return (
-    <Card className={cn("w-full flex flex-col h-full shadow-xl", className)}>
+    <Card className={cn("w-full flex flex-col h-full shadow-xl border-accent", className)}>
       {(headerContent || actions) && (
         <CardHeader className={cn(headerClassName)}>
             <div className="flex items-start justify-between">
