1f4f4e06fa4980098b83f91d06e0bba22c71ae5a
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-15 03:52:20 +0000

    The blue flex box is Fit to content.  Do not remove any flex boxes and t

--- src/components/biomarker-card.tsx
+++ src/components/biomarker-card.tsx
@@ -138,7 +138,7 @@ export function BiomarkerCard<T extends Record>({
   ) : null;
 
   const RecordsList = (
-    <div className="flex items-center border border-blue-500">
+    <div className="flex border border-blue-500">
         <ScrollArea className="h-[150px] w-full">
             <ul className="flex flex-col justify-center h-full space-y-1 mt-2">
             {formattedRecords.map((record) => (
@@ -190,7 +190,7 @@ export function BiomarkerCard<T extends Record>({
        {hasRecords ? (
           <div className="flex flex-col flex-1 h-full p-6 pt-0">
             <div className="flex flex-row items-stretch gap-4 border border-red-500 rounded-lg">
-                <div className="flex-1 pr-2 border border-green-500 flex items-center rounded-lg">
+                <div className="flex-1 pr-2 border border-green-500 flex rounded-lg">
                     {RecordsList}
                 </div>
                 <div className="pl-2 border border-yellow-500 flex flex-col justify-center items-center rounded-lg">
@@ -200,7 +200,7 @@ export function BiomarkerCard<T extends Record>({
 
             <Separator className="my-4" />
             
-            <div className="flex-1 flex w-full border border-red-500 p-2 rounded-lg">
+            <div className="flex-1 flex w-full border p-2">
                 <div className="flex-1 flex w-full min-h-[200px] border">
                     {chart}
                 </div>
