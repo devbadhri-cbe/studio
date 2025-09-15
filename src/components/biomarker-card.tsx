2ba823d24790b1ac04fdb5ccefe9c81da5b66f68
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-15 02:51:46 +0000

    Do what I instruct only. Green flex box should grow horizontally if need

--- src/components/biomarker-card.tsx
+++ src/components/biomarker-card.tsx
@@ -187,9 +187,9 @@ export function BiomarkerCard<T extends Record>({
     >
        {hasRecords ? (
           <div className="flex flex-col flex-1 h-full p-6 pt-0">
-            <div className="flex flex-row items-stretch gap-4 border border-red-500">
-                <div className="w-1/2 pr-2 border border-green-500">{RecordsList}</div>
-                <div className="w-1/2 pl-2 border border-yellow-500 flex flex-col justify-center items-center">
+            <div className="flex flex-row items-center gap-4 border border-red-500">
+                <div className="flex-1 pr-2 border border-green-500">{RecordsList}</div>
+                <div className="pl-2 border border-yellow-500 flex flex-col justify-center items-center">
                     {statusContent}
                 </div>
             </div>
