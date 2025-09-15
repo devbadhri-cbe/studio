c065a584bba25e8004dacd1b5bd34ee8474b0b21
Badhrinathan Nithyanandam <dev.badhri@gmail.com>
2025-09-15 03:43:36 +0000

    Both the record and status should be same lay out properties . Explain.

--- src/components/biomarker-card.tsx
+++ src/components/biomarker-card.tsx
@@ -189,9 +189,7 @@ export function BiomarkerCard<T extends Record>({
           <div className="flex flex-col flex-1 h-full p-6 pt-0">
             <div className="flex flex-row items-stretch gap-4 border border-red-500 rounded-lg">
                 <div className="flex-1 pr-2 border border-green-500 flex items-center rounded-lg">
-                    <ScrollArea className="h-[150px] w-full">
-                        {RecordsList}
-                    </ScrollArea>
+                    {RecordsList}
                 </div>
                 <div className="pl-2 border border-yellow-500 flex flex-col justify-center items-center rounded-lg">
                     {statusContent}
@@ -200,7 +198,7 @@ export function BiomarkerCard<T extends Record>({
 
             <Separator className="my-4" />
             
-            <div className="flex-1 flex w-full border border-red-500 p-2">
+            <div className="flex-1 flex w-full border border-red-500 p-2 rounded-lg">
                 <div className="flex-1 flex w-full min-h-[200px] border">
                     {chart}
                 </div>
